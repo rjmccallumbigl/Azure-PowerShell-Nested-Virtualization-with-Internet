539512cfecef349f4bc068b78f6e2184e7acc7f5
Ryan McCallum <rjmccallumbigl@gmail.com>
2020-08-12 13:44:08 -0400

    Updating VM name, adding comments

--- azVMRepairNestedVirtualization.ps1
+++ azVMRepairNestedVirtualization.ps1
@@ -1,23 +1,30 @@
-# az extension add -n vm-repair
-# az vm repair create -g RG_PELO_EDGE_EAST_US -n stEdg-hdfxq --verbose
+# 
+# 
+# 
+# 
+
+# Create repair VM
+
+az extension add -n vm-repair
+az vm repair create -g RG_PELO_EDGE_EAST_US -n stEdg-hdfxq --verbose
 
 #resize VM to a v3
-# az vm resize -g "repair-stEdg-hdfxq-20200807183824.638282" -n "repair-stEdg-h_" --size Standard_D2s_v3
+az vm resize -g "repair-stEdg-hdfxq-20200807183824.638282" -n "repair-stEdg-h_" --size Standard_D2s_v3
 
 # Newly created repair VM
 $vmName = "repair-WS2019T_"
 $resourceGroupName = "repair-WS2019TestingVM-20200812162035.322836"
 
 # Install Hyper V remotely, change your script path accordingly
-Invoke-AzVMRunCommand -ResourceGroupName $resourceGroupName -Name $vmName -CommandId 'RunPowerShellScript' -ScriptPath 'C:\Users\rymccall\OneDrive - Microsoft\PowerShell\installHyperV.ps1'
+Invoke-AzVMRunCommand -ResourceGroupName $resourceGroupName -Name $vmName -CommandId 'RunPowerShellScript' -ScriptPath '.\dependencies\installHyperV.ps1'
 
 # Sleep for 2 minutes until Hyper V is finished installing
 Start-Sleep -s 120
 
 # Set up Hyper V remotely, change your script path accordingly
-Invoke-AzVMRunCommand -ResourceGroupName $resourceGroupName -Name $vmName -CommandId 'RunPowerShellScript' -ScriptPath 'C:\Users\rymccall\OneDrive - Microsoft\PowerShell\provisionForNestedVirtualization.ps1'
+Invoke-AzVMRunCommand -ResourceGroupName $resourceGroupName -Name $vmName -CommandId 'RunPowerShellScript' -ScriptPath '.\dependencies\provisionForNestedVirtualization.ps1'
 
 # Create Hyper V VM remotely
-Invoke-AzVMRunCommand -ResourceGroupName $resourceGroupName -Name $vmName -CommandId 'RunPowerShellScript' -ScriptPath 'C:\Users\rymccall\OneDrive - Microsoft\PowerShell\createNewHyperVVM.ps1'
+Invoke-AzVMRunCommand -ResourceGroupName $resourceGroupName -Name $vmName -CommandId 'RunPowerShellScript' -ScriptPath '.\dependencies\createNewHyperVVM.ps1'
 
 
--- dependencies/createNewHyperVVM.ps1
+++ dependencies/createNewHyperVVM.ps1
@@ -1,11 +1,14 @@
-# Provisioning internal cloned Hyper-V VM with Nested Virtualization on Rescue VM with the following administrative PowerShell on repair-stEdg-h_:
+# Provisioning internal cloned Hyper-V VM with Nested Virtualization on Rescue VM with the following administrative PowerShell
+# https://docs.microsoft.com/en-us/windows-server/virtualization/hyper-v/get-started/create-a-virtual-machine-in-hyper-v
+
+# Get current switch, should have been created after running .\dependencies\provisionForNestedVirtualization.ps1
 $vmswitch=Get-VMSwitch
 
 # Set Hyper V VM name
-$vmName = "newRescue";
+$vmName = "newRescueVM";
 
 # Create new VM
-New-VM -Name "newRescue" -Generation 1 -MemoryStartupBytes 4096MB -SwitchName $vmswitch.Name;
+New-VM -Name $vmName -Generation 1 -MemoryStartupBytes 4096MB -SwitchName $vmswitch.Name;
 
 # Make sure Disk 2 is offline
 set-disk -number 2 -IsOffline $True;
