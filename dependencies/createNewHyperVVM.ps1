e1ad73fcce4f95d43742f6fd2244076252bbde4b
Ryan McCallum <rjmccallumbigl@gmail.com>
2020-08-13 18:10:40 -0400

    Remove vmconnect.exe since it may lock the invoke command

--- dependencies/createNewHyperVVM.ps1
+++ dependencies/createNewHyperVVM.ps1
@@ -22,6 +22,3 @@ Get-Disk 2 | Add-VMHardDiskDrive -VMName $vmName;
 
 # Start VM
 Start-VM -Name $vmName;
-
-# Connect to the virtual machine. REMOVED BECAUSE IT APPEARS TO LOCK THE INVOKE-AZVMRUNCOMMAND
-# VMConnect.exe
